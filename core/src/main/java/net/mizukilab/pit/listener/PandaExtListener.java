f74e28cfa3c27c1b580bbbcb053e1845989b1b38
MikkoAbudo <mikkoabudo@proton.me>
2025-10-05 21:12:36 +0800

    修复玩家不显示

--- core/src/main/java/net/mizukilab/pit/listener/PandaExtListener.java
+++ core/src/main/java/net/mizukilab/pit/listener/PandaExtListener.java
@@ -1,5 +1,6 @@
 package net.mizukilab.pit.listener;
 
+import net.mizukilab.pit.util.PlayerUtil;
 import net.mizukilab.pit.util.Utils;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
@@ -12,6 +13,9 @@ public class PandaExtListener implements Listener {
     public void onEntryUpdateEvent(EntityTrackerUpdateEvent e){
         Entity trackedEntity = e.getTrackedEntity();
         if(trackedEntity instanceof ArmorStand){
+            return;
+        }
+        if(!PlayerUtil.isNPC(e.getTrackedEntity())){
             return;
         }
         e.setCancelled(Utils.isInArena(e.getEntity()) != Utils.isInArena(trackedEntity));
